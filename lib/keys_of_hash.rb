3ebef507662620b95add7675c8489fc1496b8ecd
eselbeus <eselbeus@gmail.com>
2019-01-28 00:01:39 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,20 @@
+require "pry"
+
 class Hash
-  def keys_of(arguments)
+  def keys_of(*arguments)
     # code goes here
+    arr = []
+    # binding.pry
+    arguments.each do |arg|
+      # binding.pry
+      # Hash.new(arg).each do |k, v|
+      #   if v == arg
+      #     arr.push(k)
+        end
+      end
+    end
+    return arr
   end
 end
+
+# arr = Hash.keys_of(*arguments)
